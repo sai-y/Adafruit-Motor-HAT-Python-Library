9d2fd2bb6bcd20106f2dceb5772aa900839648a2
Sai Yamanoor <sai@altschool.com>
2016-12-13 18:48:35 -0800

    Update Adafruit_I2C.py

--- Adafruit_MotorHAT/Adafruit_I2C.py
+++ Adafruit_MotorHAT/Adafruit_I2C.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+from __future__ import print_function
 import re
 import smbus
-from __future__ import print_function
 
 # ===========================================================================
 # Adafruit_I2C Class
